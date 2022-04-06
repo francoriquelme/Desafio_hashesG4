e5e182ee9a6b0520f0321f4c1a1a09abd7cdd278
francoriquelme <francoestebanru@gmail.com>
2022-04-05 21:25:43 -0400

    finished

--- trimestre.rb
+++ trimestre.rb
@@ -20,8 +20,19 @@ Diciembre: 21000
 # ruby trimestres.rb
 # {"Q1"=>49000, "Q2"=>111000, "Q3"=>87200, "Q4"=>133500}
 
-ventas.each_slice(3) { |a| print a}
+
+# /////// Una forma de hacerlo eficiente ///////
+# ventas.values.each_slice(3) { |a| print a}
 # [[:Enero, 15000], [:Febrero, 22000], [:Marzo, 12000]][[:Abril, 17000], [:Mayo, 81000], 
 # [:Junio, 13000]][[:Julio, 21000], [:Agosto, 41200], [:Septiembre, 25000]][[:Octubre, 21500], [:Noviembre, 91000], 
 # [:Diciembre, 21000]]
+
+
+# ///////// Otra forma de hacerlo /////////
+# quarters = {}
+
+ventas.values.each_slice(3).each_with_index do |element, index|
+    quarters["Q#{index + 1}"] = element.sum
+end
+print quarters
 
